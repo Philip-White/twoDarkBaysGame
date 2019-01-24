189ae84d5cda3ded943d0bcbd06a0547667e49ca
Unrising <phlwhite5@gmail.com>
2019-01-24 16:17:31 -0400

    modified:   assets/menu.js 	new file:   assets/tilesets/Two-Dark-BaysWhiteBG.png 	new file:   assets/tilesets/woodenFloor.jpg 	modified:   index.html

--- assets/menu.js
+++ assets/menu.js
@@ -5,9 +5,16 @@ class menu extends Phaser.Scene{
     super({key:"menu"});
   }
 
+  preload(){
+    this.load.image("woodenFloor", "../assets/tilesets/woodenFloor.jpg");
+    this.load.image("Two-Dark-BaysWhiteBG", "../assets/tilesets/Two-Dark-BaysWhiteBG.png");
+  }
+
 
 create (){
-this.text = this.add.text(0,0, "Menu Screen", {font: "40px impact"});
+  this.add.image(400,300, "woodenFloor").setScale(.3);
+  this.add.image(400, 300, "Two-Dark-BaysWhiteBG").setScale(.7);
+this.text = this.add.text(270,500, "Two Dark Bays Game", {font: "30px impact", color: "black"});
 /*this is another way to use the keys on the keyboard.  https://www.youtube.com/watch?v=7cpZ5Y7THmo&t=763s
 quicker so you don't have to make the update funcion as well.
 */
