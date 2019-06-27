0e5987a2135b5c273d20f604d4c20df0d6138b9b
Philip White <phlwhite5@gmail.com>
2019-06-27 10:58:47 -0300

    modified:   assets/instructions.js 	new file:   assets/tilesets/ jump1.png 	new file:   assets/tilesets/joyinstruct.png 	new file:   assets/tilesets/joyinstruct.xcf 	new file:   assets/tilesets/talkingHorse.png 	modified:   assets/tilesets/voice.png 	new file:   assets/tilesets/voice.xcf

--- assets/instructions.js
+++ assets/instructions.js
@@ -1,6 +1,10 @@
 let texts;
 let skip;
 let camera1;
+let jumpButtons;
+let arsehole;
+let joyinstruct;
+
 
 class instructions extends Phaser.Scene{
   constructor () {
@@ -58,12 +62,16 @@ class instructions extends Phaser.Scene{
                          this.load.image("slideBack", "../assets/tilesets/slideBack.png");
                          this.load.image("jump", "../assets/tilesets/jump.png");
                          this.load.image("voice", "../assets/tilesets/voice.png");
-
+this.load.image("joys", "../assets/tilesets/joyinstruct.png");
+this.load.image("talkingHorse", "../assets/tilesets/talkingHorse.png");
 }
 
 create(){
   let camera1 = this.cameras.main.fadeIn(1000);
   this.input.addPointer(3);
+
+
+
 
 function hello(){
   camera1.fade(1000);
@@ -71,12 +79,23 @@ function hello(){
 }
 
   this.add.image(400, 300, "slideBack").setScale(1.4);
-  skip = this.add.sprite(450, 280, "voice").setScale(.33).setScrollFactor(0).setInteractive();
+  this.add.image(290, 280, "talkingHorse");
+  skip = this.add.image(485, 200, "voice").setScale(.33).setScrollFactor(0).setInteractive();
+  jumpButtons = this.add.image(700, 525, "jump").setScale(.60).setScrollFactor(0).setInteractive();
+joyinstruct = this.add.image(100, 525, "joys");
 
-  let texts = this.add.text(480, 35, `${coinScore}well this is just a test to see how much text I can put in this thing before it gets all fucked up......Lets just keep it going and see how far we can push it!`, {
+
+
+  //this self variable is making a reference for future functions to address with 'this'..
+var self = this;
+
+
+
+
+  let texts = this.add.text(380, 135, `Welcome to Two Dark Bays the game!  Jump the rails with RickyBobby and collect as many of the donuts as you can!  (click to continue)`, {
         font: '16px Arvo',
         fill: 'black',
-        wordWrap: { width: 225, height: 225, useAdvancedWrap: true }
+        wordWrap: { width: 225, height: 230, useAdvancedWrap: true }
       });
       texts.setScrollFactor(.05);
 
@@ -89,16 +108,40 @@ function hello(){
 
   skip.on('pointerdown', function (event){
 
-texts.setText("Well hey!  Yeah do you want the money or should I just shove it directly up your fat ass?!");
+texts.setText("If you are using a desktop, navigate with the arrow keys.  To Jump, tap and release the 's' key!(click to continue)");
 skip.on('pointerdown', function (event){
-texts.setText("Oh yeah!..  Oh yeah!");
-skip.on('pointerdown', function (event){
+texts.setText("On mobile, you can navigate with the joystick on the bottom left corner of your screen!  (click to continue)");
 
+var tween1 = self.tweens.add({
+  targets: joyinstruct,
+  y: 450,
+  duration: 3000,
+  ease: 'Elastic',
+  easeParams: [ 1.5, 0.5 ],
+})
+
+
+
+skip.on('pointerdown', function (event){
+  texts.setText("Also on mobile, you can jump RickyBobby with this button in the bottom right corner of your screen! (click to start playing)");
+
+
+  var tween = self.tweens.add({
+      targets: jumpButtons,
+      y: 450,
+      duration: 3000,
+      ease: 'Elastic',
+      easeParams: [ 1.5, 0.5 ],
+  });
+
+
+
+  skip.on('pointerdown', function (event){
 hello();
-
 });
 });
 });
+}, this);
 
 
 
@@ -120,5 +163,6 @@ hello();
   }, this);
 */
 }
+
 
 }
